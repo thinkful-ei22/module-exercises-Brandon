e18b9d07cda263769a8297ab8ddeb5d46818231f
Tyler Warrick <tcw5038@gmail.com>
2018-06-18 09:31:42 -0700

    Added part 2 of exercise

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global store, cuid */
+/* global store, Item, cuid */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -57,7 +57,16 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);
+      Item.create(itemName);
+      store.items.push({ id: cuid(), name: itemName, checked: false });
+      render();
+    }
+    catch(e){
+      console.log('Cannot add item:{error.message');
+    }
+    
   }
   
   function handleNewItemSubmit() {
